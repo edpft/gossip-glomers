31b98641ed385abc213bc88f62246f64ff8d4e69
Ed Fawcett-Taylor <65284206+edpft@users.noreply.github.com>
2023-09-16 18:39:24 +0100

    feat(echo): implement "echo" message handling (#1)
    * chore: ignore dir created by `maelstrom` tests
    * chore(deps): add `color_eyre` to deps
    * chore(deps): add thiserror
    * feat(echo): implement "echo" message handling
    * chore: use re-usable workflows
    * chore: add dependabot version updates

---
+++ tests/maelstrom.rs
@@ -0,0 +1,21 @@
+use std::process::Command;
+
+#[test]
+fn test_handle_echo() -> color_eyre::Result<()> {
+    let status = Command::new("./maelstrom/maelstrom")
+        .args([
+            "test",
+            "-w",
+            "echo",
+            "--bin",
+            "./target/debug/gossip-glomers",
+            "--node-count",
+            "1",
+            "--time-limit",
+            "10",
+        ])
+        .status()
+        .expect("failed to execute process");
+    assert!(status.success());
+    Ok(())
+}
