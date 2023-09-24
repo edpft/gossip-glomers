fed5adb31d25fc441a3c94982e3385a1bc1f15db
Ed Fawcett-Taylor <edwardpaultaylor@gmail.com>
2023-09-24 20:11:28 +0100

    refactor: re-implement `Node` as a state machine and `Message` as a transition.

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,37 @@
-mod error;
 mod message;
-mod server;
+mod node;
 
-use std::{convert::TryFrom, io};
+use std::{
+    io,
+    time::{Duration, Instant},
+};
 
 use serde_json::Deserializer;
-use server::Server;
 use tracing::info;
 use tracing_subscriber::filter::LevelFilter;
 
-use crate::message::Message;
+use crate::{message::Message, node::Node};
 
 fn main() -> color_eyre::Result<()> {
     initialise_tracing();
+    let mut node = Node::new();
 
     let stdin = io::stdin();
     info!("Got stdin");
 
-    let mut buf = String::new();
-    stdin.read_line(&mut buf)?;
-
-    let initial_request: Message = serde_json::from_str(&buf)?;
-    info!(target: "Received request", request = ?initial_request);
-
-    let mut server = Server::try_from(&initial_request)?;
-    info!("Initialised server");
-
-    let initial_response = server.handle_initial_request(initial_request)?;
-
-    println!("{}", &initial_response);
-    info!(target: "Sent response", response = ?initial_response);
-
     let requests = Deserializer::from_reader(stdin).into_iter::<Message>();
-    requests.flatten().enumerate().for_each(|(index, request)| {
-        info!(target: "Received request", request = ?request);
-        if let Ok(Some(response)) = server.handle_request(request) {
-            println!("{}", &response);
-            info!(target: "Sent response", response = ?response);
-        };
 
-        if index % 4 == 0 {
-            if let Some(messages) = server.generate_gossip() {
-                messages.iter().for_each(|message| {
-                    println!("{}", &message);
-                    info!(target: "Sent request", request = ?message);
-                })
-            }
-        };
-    });
+    let duration = Duration::from_millis(300);
+    let mut now = Instant::now();
 
-    if let Some(messages) = server.generate_gossip() {
-        messages.iter().for_each(|message| {
-            println!("{}", &message);
-            info!(target: "Sent request", request = ?message);
-        })
+    for request in requests.flatten() {
+        node = node.handle(request);
+        if now.elapsed() > duration {
+            node.gossip();
+            now = Instant::now();
+        }
     }
+    node.gossip();
     Ok(())
 }
 
