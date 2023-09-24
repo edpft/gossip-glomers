805a7950fd3c87cbee04ccf78cb98de77e36545e
Ed Fawcett-Taylor <edwardpaultaylor@gmail.com>
2023-09-24 20:57:03 +0100

    test: add fault tolerant broadcast test

--- tests/maelstrom.rs
+++ tests/maelstrom.rs
@@ -89,3 +89,27 @@ fn test_handle_multi_node_broadcast() -> color_eyre::Result<()> {
     assert!(status.success());
     Ok(())
 }
+
+#[test]
+fn test_handle_fault_tolerant_broadcast() -> color_eyre::Result<()> {
+    let status = Command::new("./maelstrom/maelstrom")
+        .args([
+            "test",
+            "-w",
+            "broadcast",
+            "--bin",
+            "./target/debug/gossip-glomers",
+            "--node-count",
+            "5",
+            "--time-limit",
+            "20",
+            "--rate",
+            "10",
+            "--nemesis",
+            "partition",
+        ])
+        .status()
+        .expect("failed to execute process");
+    assert!(status.success());
+    Ok(())
+}
