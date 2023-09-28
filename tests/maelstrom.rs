e810370ccf79cee8f678de34139ad93a66a90cd5
Ed Fawcett-Taylor <65284206+edpft@users.noreply.github.com>
2023-09-28 22:29:36 +0100

    feat(broadcast): Implement efficient broadcast 2 (#23)
    ci: add `broadcast_efficiency_2` to matrix

--- tests/maelstrom.rs
+++ tests/maelstrom.rs
@@ -160,3 +160,48 @@ fn test_broadcast_efficiency_1() -> color_eyre::Result<()> {
     assert!(maximum_latency < 600.0);
     Ok(())
 }
+
+#[test]
+fn test_broadcast_efficiency_2() -> color_eyre::Result<()> {
+    let output = Command::new("./maelstrom/maelstrom")
+        .args([
+            "test",
+            "-w",
+            "broadcast",
+            "--bin",
+            "./target/debug/gossip-glomers",
+            "--node-count",
+            "25",
+            "--time-limit",
+            "20",
+            "--rate",
+            "100",
+            "--latency",
+            "100",
+        ])
+        .output()
+        .expect("failed to execute process");
+    let stdout = String::from_utf8(output.stdout)?;
+    let results_line: String = stdout
+        .lines()
+        .filter(|line| line.contains("jepsen.store"))
+        .collect();
+    let re = Regex::new(r"jepsen.store Wrote (.*results.edn)$")?;
+    let captures = re.captures(&results_line).unwrap();
+    let path = captures.get(1).unwrap().as_str();
+    let content = fs::read_to_string(path)?;
+    let edn = Edn::from_str(&content)?;
+    let messages_per_operation = edn[":net"][":all"][":msgs-per-op"]
+        .to_float()
+        .expect("Cast msgs-per-op to float");
+    assert!(messages_per_operation < 20.0);
+    let median_latency = edn[":workload"][":stable-latencies"]["0.5"]
+        .to_float()
+        .expect("Cast median latency to float");
+    assert!(median_latency < 1000.0);
+    let maximum_latency = edn[":workload"][":stable-latencies"]["1"]
+        .to_float()
+        .expect("Cast maximum latency to float");
+    assert!(maximum_latency < 1000.0);
+    Ok(())
+}
