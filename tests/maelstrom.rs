6e2c8270a2ed3b635d9de8e9c69972d6cc8d7c59
Ed Fawcett-Taylor <edwardpaultaylor@gmail.com>
2023-09-28 21:52:37 +0100

    test: extract key metrics from results

--- tests/maelstrom.rs
+++ tests/maelstrom.rs
@@ -1,4 +1,6 @@
-use std::process::Command;
+use edn_rs::Edn;
+use regex::Regex;
+use std::{fs, process::Command, str::FromStr};
 
 #[test]
 fn test_echo() -> color_eyre::Result<()> {
@@ -116,7 +118,7 @@ fn test_fault_tolerant_broadcast() -> color_eyre::Result<()> {
 
 #[test]
 fn test_broadcast_efficiency_1() -> color_eyre::Result<()> {
-    let status = Command::new("./maelstrom/maelstrom")
+    let output = Command::new("./maelstrom/maelstrom")
         .args([
             "test",
             "-w",
@@ -132,8 +134,29 @@ fn test_broadcast_efficiency_1() -> color_eyre::Result<()> {
             "--latency",
             "100",
         ])
-        .status()
+        .output()
         .expect("failed to execute process");
-    assert!(status.success());
+    let stdout = String::from_utf8(output.stdout)?;
+    let results_line: String = stdout
+        .lines()
+        .filter(|line| line.contains("jepsen.store"))
+        .collect();
+    let re = Regex::new(r"jepsen.store Wrote (.*results.edn)$")?;
+    let captures = re.captures(&results_line).unwrap();
+    let path = captures.get(1).unwrap().as_str();
+    let content = fs::read_to_string(path)?;
+    let edn = Edn::from_str(&content)?;
+    let messages_per_operation = edn[":net"][":all"][":msgs-per-op"]
+        .to_float()
+        .expect("Cast msgs-per-op to float");
+    assert!(messages_per_operation < 30.0);
+    let median_latency = edn[":workload"][":stable-latencies"]["0.5"]
+        .to_float()
+        .expect("Cast median latency to float");
+    assert!(median_latency < 400.0);
+    let maximum_latency = edn[":workload"][":stable-latencies"]["1"]
+        .to_float()
+        .expect("Cast maximum latency to float");
+    assert!(maximum_latency < 600.0);
     Ok(())
 }
